8ced8d0e4fb89a5baa76e4ac6fafc1552481d991
Ajeet Singh <46717261+raba-ajeet@users.noreply.github.com>
2020-09-02 23:46:01 +0530

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,7 +1,7 @@
 const int N=5*(1e5)+10;
 int par[N],ran[N];
 void make_set(int n){
-    for(int i-0;i<n;i++) par[i]=i,ran[i]=0;
+    for(int i=0;i<n;i++) par[i]=i,ran[i]=0;
 }
 int find_set(int u){
     if(par[u]==u) return u;
