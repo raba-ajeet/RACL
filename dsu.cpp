2dd6f992503ae0578af793b8f1fa26a2aa49a1d3
Ajeet Singh <46717261+raba-ajeet@users.noreply.github.com>
2020-09-02 23:44:24 +0530

    Create dsu.cpp

---
+++ dsu.cpp
@@ -0,0 +1,17 @@
+const int N=5*(1e5)+10;
+int par[N],ran[N];
+void make_set(int n){
+    fi(i,n+1) par[i]=i,ran[i]=0;
+}
+int find_set(int u){
+    if(par[u]==u) return u;
+    return par[u]=find_set(par[u]);
+}
+void union_set(int u,int v){
+    int x=find_set(u);
+    int y=find_set(v);
+    if(x==y) return ;
+    if(ran[x]<ran[y]) swap(x,y);
+    par[y]=x;
+    if(ran[x]!=ran[y]) ran[x]++;
+}
